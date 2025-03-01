87a16a53657535420f45c2a0b59f50a1c2c3c2e6
Sarah Drury <drurys1@cardiff.ac.uk>
2025-03-01 17:53:29 +0000

    Added tasks.html, made changes to routes.py and database

--- webapp_prototype/portfolio/SQLs/schema.sql
+++ webapp_prototype/portfolio/SQLs/schema.sql
@@ -13,17 +13,20 @@ CREATE TABLE IF NOT EXISTS "tasks"(
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "task_name" TEXT NOT NULL,
     "task_content" TEXT,
-    "task_icon" TEXT NOT NULL
+    "task_icon" TEXT NOT NULL,
+    "active" INTEGER NOT NULL check(active = 1 OR active = 0),
+    "user_id" INTEGER NOT NULL,
+    FOREIGN KEY ("user_id") REFERENCES "users" ("id")
 );
 
-CREATE TABLE IF NOT EXISTS "assigned"(
-    "user_id" INTEGER NOT NULL,
-    "task_id" INTEGER NOT NULL,
-    "active" INTEGER NOT NULL check(active = 1 OR active = 0),
-    "order" INTEGER NOT NULL,
-    PRIMARY KEY ("user_id", "task_id"),
-    FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
-    FOREIGN KEY ("task_id") REFERENCES "tasks" ("id")
-); 
+-- CREATE TABLE IF NOT EXISTS "assigned"(
+--     "user_id" INTEGER NOT NULL,
+--     "task_id" INTEGER NOT NULL,
+--     "active" INTEGER NOT NULL check(active = 1 OR active = 0),
+--     "order" INTEGER NOT NULL,
+--     PRIMARY KEY ("user_id", "task_id"),
+--     FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
+--     FOREIGN KEY ("task_id") REFERENCES "tasks" ("id")
+-- ); 
 
 
